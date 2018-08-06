8eb76070458d229d15df80d89f1753f2d6be9d55
kenny <k@kenny.cc>
2018-08-06 17:50:04 +0800

    update

---
+++ kit/list/nav.js
@@ -0,0 +1,20 @@
+Component({
+  properties: {
+    icon: {
+      type: String,
+      value: ''
+    },
+    url: {
+      type: String,
+      value: ''
+    },
+    title: {
+      type: String,
+      value: ''
+    },
+    content: {
+      type: String,
+      value: ''
+    },
+  },
+})
