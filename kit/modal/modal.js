e47feb050263b8e13200638e6f1edbe01137a8e0
kenny <k@kenny.cc>
2018-08-21 11:24:55 +0800

    update

---
+++ kit/modal/modal.js
@@ -0,0 +1,16 @@
+Component({
+  properties: {
+    show: {
+      type: Boolean,
+      value: false,
+    },
+    width: {
+      type: Number,
+      value: 600
+    },
+    height: {
+      type: Number,
+      value: 350
+    }
+  }
+})
